6b460176883dbb6807c0b03c74b41d8a0d8e2a63
Waga <wagaodongo@gmail.com>
2019-12-11 15:16:34 +0300

    Color

--- RGB/ContentView.swift
+++ RGB/ContentView.swift
@@ -9,22 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
     var body: some View {
-        HStack {
-            VStack {
-                /*@START_MENU_TOKEN@*/Color(red: 0.5, green: 0.5, blue: 0.5)/*@END_MENU_TOKEN@*/
-                Text("Match this color")
+        
+        
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    Text("Match this color")
+                }
+                VStack {
+                    /*@START_MENU_TOKEN@*/Color(red: 0.5, green: 0.5, blue: 0.5)/*@END_MENU_TOKEN@*/
+                    Text("R:127 G:127 B:127")
+                }
             }
-            VStack {
-                /*@START_MENU_TOKEN@*/Color(red: 0.5, green: 0.5, blue: 0.5)/*@END_MENU_TOKEN@*/
-                Text("R:127 G:127 B:127")
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
             }
+            
+            Slider(value: .constant(0.5))
+
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().previewLayout(.fixed(width: 568, height: 320))
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5).previewLayout(.fixed(width: 568, height: 320))
     }
 }
