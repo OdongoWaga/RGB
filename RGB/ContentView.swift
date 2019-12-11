66ca8acc313e16644865d04a411701cfe393639f
Waga <wagaodongo@gmail.com>
2019-12-11 14:28:01 +0300

    Initial Commit

---
+++ RGB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGB
+//
+//  Created by JFJ on 11/12/2019.
+//  Copyright © 2019 JFJ. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
